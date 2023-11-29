c20d5f77e9f0f4ed6ddec3297ad2a2854bc53629
Francisca Cardemil <107263145+frcardemil@users.noreply.github.com>
2023-11-29 11:10:55 -0300

    Add files via upload

--- UmamiUrbano/src/app/pages/inicio/inicio-routing.module.ts
+++ UmamiUrbano/src/app/pages/inicio/inicio-routing.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { InicioPage } from './inicio.page';
+import { InicioComponent } from 'src/app/components/inicio/inicio.component';
+import { BuscarComponent } from 'src/app/components/buscar/buscar.component';
+import { EscribirComponent } from 'src/app/components/escribir/escribir.component';
+import { PerfilComponent } from 'src/app/components/perfil/perfil.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: InicioPage
+    component: InicioPage,
+    children: [
+      {
+        path: '',
+        component: InicioComponent,
+      },
+      {
+        path: 'buscar',
+        component: BuscarComponent,
+      },
+      {
+        path: 'escribir',
+        component: EscribirComponent,
+      },
+      {
+        path: 'perfil',
+        component: PerfilComponent,
+      },
+    ],
   }
 ];
 
